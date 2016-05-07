539227b4a32dca5095cc2e73b332109afaba445a
Yury Selivanov <yury@magic.io>
2016-05-06 21:49:57 -0400

    Bump version to v0.4.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, Extension
 
 setup(
     name='httptools',
-    version='0.0.6',
+    version='0.0.7',
     description='A collection of framework independent HTTP protocol utils.',
     classifiers=[
         'License :: OSI Approved :: MIT License',
