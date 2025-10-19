623b8feaebc71f145080fd686fc1b9284c035bbd
Marcelo Trylesinski <marcelotryle@gmail.com>
2025-10-19 18:21:02 +0200

    push

--- httptools/parser/url_parser.pyi
+++ httptools/parser/url_parser.pyi
@@ -11,16 +11,4 @@ class URL:
     userinfo: bytes
 
 def parse_url(url: Union[bytes, bytearray, memoryview, array[int]]) -> URL:
-    """Parse URL strings into a structured Python object.
-
-    Returns an instance of ``httptools.URL`` class with the
-    following attributes:
-
-      - schema(bytes): The schema of the URL.
-      - host(bytes): The host of the URL.
-      - port: int
-      - path(bytes): The path of the URL.
-      - query(bytes): The query of the URL.
-      - fragment(bytes): The fragment of the URL.
-      - userinfo(bytes): The userinfo of the URL.
-    """
+    """Parse a URL string into a structured Python object."""
